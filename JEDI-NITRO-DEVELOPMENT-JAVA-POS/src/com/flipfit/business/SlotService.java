a7f38ba76a87de0109bb3be0ddcf267abb7f07f2
S-S-Poojitha <sarvamangalapoojitha@gmail.com>
2026-01-26 21:36:08 +0530

    line 17 fix in SlotService

--- JEDI-NITRO-DEVELOPMENT-JAVA-POS/src/com/flipfit/business/SlotService.java
+++ JEDI-NITRO-DEVELOPMENT-JAVA-POS/src/com/flipfit/business/SlotService.java
@@ -4,62 +4,23 @@ import com.flipfit.bean.Slot;
 import java.time.LocalTime;
 import java.util.List;
 
-/**
- * Implementation of SlotServiceInterface.
- */
-public class SlotService implements SlotServiceInterface {
+public interface SlotService {
     
-    @Override
-    public Slot createSlot(int centerId, LocalTime startTime, LocalTime endTime, int totalSeats) {
-        // TODO: Implement
-        return null;
-    }
+    public Slot createSlot(int centerId, LocalTime startTime, LocalTime endTime, int totalSeats);
     
-    @Override
-    public Slot getSlotById(int slotId) {
-        // TODO: Implement
-        return null;
-    }
-    
-    @Override
-    public List<Slot> getSlotsByCenter(int centerId) {
-        // TODO: Implement
-        return null;
-    }
-    
-    @Override
-    public List<Slot> getAvailableSlotsByCenter(int centerId) {
-        // TODO: Implement
-        return null;
-    }
-    
-    @Override
-    public boolean isSlotAvailable(int slotId) {
-        // TODO: Implement
-        return false;
-    }
-    
-    @Override
-    public boolean lockSlot(int slotId) {
-        // TODO: Implement
-        return false;
-    }
-    
-    @Override
-    public boolean unlockSlot(int slotId) {
-        // TODO: Implement
-        return false;
-    }
-    
-    @Override
-    public boolean updateSlot(int slotId, LocalTime startTime, LocalTime endTime, int totalSeats) {
-        // TODO: Implement
-        return false;
-    }
-    
-    @Override
-    public boolean deleteSlot(int slotId) {
-        // TODO: Implement
-        return false;
-    }
+    public Slot getSlotById(int slotId);
+
+    public List<Slot> getSlotsByCenter(int centerId);
+
+    public List<Slot> getAvailableSlotsByCenter(int centerId);
+
+    public boolean isSlotAvailable(int slotId);
+
+    public boolean lockSlot(int slotId);
+
+    public boolean unlockSlot(int slotId);
+
+    public boolean updateSlot(int slotId, LocalTime startTime, LocalTime endTime, int totalSeats);
+
+    public boolean deleteSlot(int slotId);
 }
